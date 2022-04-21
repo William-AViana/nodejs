dbc51b6f10a75386003dd631a86b517b0f4b1ac6
William Viana <willdev.viana@gmail.com>
2022-04-20 22:45:04 -0300

    create server for read request on url

---
+++ getUrl/getUrl.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Contente-Type': 'text/html' });
+  res.write(req.url);
+
+  res.write('<br>')
+
+  const q = url.parse(req.url, true).query
+  let txt = q.year + " " + q.month;
+
+  res.end(txt);
+}).listen(3000);
