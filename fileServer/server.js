ee767f058d02f2c939565cd8c98e1cbaac71f533
William Viana <willdev.viana@gmail.com>
2022-04-27 21:50:56 -0300

    create server for read files on directory

---
+++ fileServer/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var fileName = '.' + q.pathname;
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      return res.end('404 not found');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  });
+}).listen(3000)
